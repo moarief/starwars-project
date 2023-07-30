8978fbbb2a1ba6a7ccf7455cb80e8708a311ac45
MoActive <arief@live.dk>
2023-07-30 21:30:19 +0200

    feat: :tada: Add redux
    Setup redux store

---
+++ lib/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+
+export const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
